24d75e202361ce77e4a2df9fe11c3b3fce2fbf1b
slarsendisney <s.larsendisney@gmail.com>
2019-10-11 22:45:50 +0100

    add sld-comps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Hero, GetThis } from "sld-component-library";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Hero from "../components/Hero";
 import Example from "../components/Example";
+import Places from "../data/Start.json";
 
 export default function Start() {
   return (
     <Layout>
       <SEO title="Home" />
       <div className="is-grey is-green-bg">
-        <Hero />
+        <Hero places={Places} title="Pear" />
+      </div>
+      <div className="is-white-bg is-grey">
+        <GetThis flag="pear" />
       </div>
       <div className="is-white is-light-grey-bg">
         <Example />
