485a079b10a7d06d880be4d5619f9cbaf3e3ffff
slarsendisney <s.larsendisney@gmail.com>
2019-10-11 16:26:58 +0100

    updated example

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const http = require("http");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 const compression = require("compression");
 
 const dev = !(process.env.NODE_ENV === "production");
 
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(compression());
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
+
+app.get("/hello", function(req, res) {
+  res.send({ message: "🍐 Hello from the server!" });
+});
 
 if (dev) {
   require("dotenv").config();
--- src/components/Example.js
+++ src/components/Example.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Example = () => (
-  <div className="row container-small pad-20-t pad-20-b is-grey pad-10-l">
-    <div className="col-xs-12">
-      <h1 className="is-hero-sub-menu margin-0">Example</h1>
+const Example = () => {
+  const [response, setResponse] = useState();
+
+  const sayHello = () => {
+    fetch("http://localhost:3000/hello")
+      .then(response => response.json())
+      .then(data => {
+        setResponse(data);
+      })
+      .catch(err => setResponse(err));
+  };
+
+  return (
+    <div className="row container-small pad-20-t pad-20-b is-grey pad-10-l">
+      <div className="col-xs-12">
+        <h1 className="is-hero-sub-menu margin-0">Example</h1>
+      </div>
+      <div className="col-xs-12">
+        <h4>
+          Find this component in{" "}
+          <code className="is-green">src/components/Example.js</code>
+        </h4>
+        <h4>This is a GET request from our UI to the server:</h4>
+        <code className="pad-3 is-grey-bg is-white border-radius">
+          {`
+            fetch("http://localhost:3000/hello");
+          `}
+        </code>
+        <h4>Trigger it by hitting the button below.</h4>
+        <button className="btn" onClick={() => sayHello()}>
+          Send Request
+        </button>
+        {response && (
+          <>
+            <h4>🎉 We got a response from the server!</h4>
+            <code className="pad-3 is-grey-bg is-white border-radius">
+              {JSON.stringify(response)}
+            </code>
+          </>
+        )}
+      </div>
     </div>
-    <div className="col-xs-12">
-      <h4>
-        Find this component in{" "}
-        <code className="is-pink">src/components/SocketExample.js</code>
-      </h4>
-      <h4>This is our dispatch action:</h4>
-      <code className="pad-3 is-grey-bg is-white border-radius">
-        {`sayHello: () => dispatch({ type: "server/hello", data: "Hello!" })`}
-      </code>
-      <h4>
-        Because it is of type "server/*", it emits to the server via the socket.
-        Trigger it by hitting the button below.
-      </h4>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Example;
