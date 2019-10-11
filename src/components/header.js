e91a91c900fa5b42873233260b413987f8041b67
slarsendisney <s.larsendisney@gmail.com>
2019-10-11 16:02:07 +0100

    updated alt text

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ export default () => (
   <div className="is-white-bg margin-5">
     <div className="row flex padding-0-tb container-small">
       <div className="col-xs-9 flex">
-        <img src={Logo} className="logo" />
+        <img src={Logo} alt="pear" className="logo" />
       </div>
     </div>
   </div>
